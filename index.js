71bdf919fb9a29f7462d865994d8428eb65fb43f
Mauricio Medeiros <mauriciomedeiros.05@gmail.com>
2017-06-05 23:08:42 -0300

    Exemple os using inheritance

--- index.js
+++ index.js
@@ -20,8 +20,33 @@ class Message {
   }
 }
 
+class ImageMessage extends Message {
+  constructor(text = '', created = Date.now(), url = '', thumbnail = '') {
+    super(text, created);
+    this.url = url;
+    this.thumbnail = thumbnail;
+  }
+
+  toString() {
+    return `Photo ${super.toString()} - Url: ${this.url} - Thumbnail: ${this.thumbnail}`;
+  }
+}
+var text = 'Some text';
+var created = Date.now();
+var duckTypeMessage = {
+  text, created
+}
+console.log(duckTypeMessage);
+
 var emptyMessage = new Message();
 var textMessage = new Message('Yesterday message', Date.now() - 86400);
+var photoMessage = new ImageMessage();
 
 console.log(String(emptyMessage));
 console.log(String(textMessage));
+console.log(String(photoMessage));
+
+console.log(emptyMessage instanceof Message);
+console.log(textMessage instanceof Message);
+console.log(photoMessage instanceof Message);
+console.log(photoMessage instanceof ImageMessage);
